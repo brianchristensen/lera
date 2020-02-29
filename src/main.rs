ae7f63c7fb904f7b3c86a5486457588fb7dde8c7
Brian Christensen <brianchristensen@outlook.com>
2020-02-29 13:54:13 -0800

    init project and debug config

---
+++ src/main.rs
@@ -0,0 +1,34 @@
+extern crate rand;
+mod map;
+
+use map::premade_map;
+use std::sync::mpsc::channel;
+use std::thread;
+use std::io;
+use rand::prelude::*;
+
+fn main() {
+    let mut rng = thread_rng();
+    let m = premade_map()[rng.gen_range(0, 9)][rng.gen_range(0, 9)];
+    println!("{}", m);
+
+    let (tx, rx) = channel::<String>();
+
+    thread::spawn(move|| {
+        loop {
+            let mut buffer = String::new();
+            io::stdin().read_line(&mut buffer).unwrap();
+            tx.send(buffer.clone()).unwrap();
+        }
+    });
+
+    loop {
+        let raw_cmd = rx.recv().unwrap().to_lowercase().replace("\n", "");
+        let cmd = raw_cmd.as_str();
+
+        match cmd {
+            "quit" | "exit" => break,
+            _ => println!("CMD: {}", cmd)
+        }
+    }
+}
